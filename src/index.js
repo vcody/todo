0c769e7b5f1cb1e13ca3265adeec44ad3e0b2e58
vcody <codyyv@protonmail.com>
2021-07-04 14:50:05 -0700

    Implemented add/remove todo functions

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+const DATA = [
+
+];
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App tasks={DATA} />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
